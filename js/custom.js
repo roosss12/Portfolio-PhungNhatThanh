7495c932e0e0e421cbc9fc837e0d24818a40aa14
PhungNhatThanh <148028219+roosss12@users.noreply.github.com>
2024-04-10 18:10:47 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+               	strings: ["Web Designer", "Graphic Designer","Digital Painting"],
+       	 	typeSpeed:100,
+        		backSpeed:100,
+        		loop:true,
+        		});
+
+new WOW().init()
